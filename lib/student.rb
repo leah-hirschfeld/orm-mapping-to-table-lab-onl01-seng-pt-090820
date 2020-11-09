9a2753c1a6d844946a97ce2ed0f6e8d2c27b89f0
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-09 21:19:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize
-    #@id = id
-    #@name = name
-    #@grade = grade
+  def initialize(name, grade)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
 end
