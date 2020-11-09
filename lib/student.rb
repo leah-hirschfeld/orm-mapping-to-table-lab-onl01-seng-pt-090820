a692ff6b8981c4d2e0f46b2ecdaa6c38f99342f6
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-09 21:22:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,4 +19,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
 end
